2bd92ff7b49902e5b5d850e0ca95b5041eddadbf
moonhr <moonhr153@gmail.com>
2024-05-14 23:41:49 +0900

    애니메이션 연습 성공

--- 5월/cat.0513/public/index.js
+++ 5월/cat.0513/public/index.js
@@ -4,7 +4,7 @@ const root = document.getElementById('root');
 function catAppend(cats){
 
   cats = [];
-
+  
   for(let i = 0; i<6; i++){
     let div = document.createElement('div');
     root.appendChild(div);
@@ -16,10 +16,21 @@ function catAppend(cats){
     cats.push(div);
     }
   }
-  setInterval((cats) => {
-
-  }, 500);
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  let catCounter = 0;
+
+  setInterval(() => {
+  // 이전 텍스트를 숨김
+    h1[catCounter].style.display = 'none'; 
+    // 증가 및 다음 텍스트로 이동, h1.length와 같다면 0
+    catCounter = (catCounter === h1.length - 1) ? 0 : catCounter + 1; 
+    // 새로운 텍스트를 표시
+    h1[catCounter].style.display = 'inline'; 
+    // 1초 간격으로 반복
+  }, 1000); 
+});
 
 
 
