3ed60c901b84b0a97558f8e221163701ccb946bc
moonhr <moonhr153@gmail.com>
2024-05-15 02:00:26 +0900

    고양이 애니메이션 성공

--- 5월/cat.0513/public/index.js
+++ 5월/cat.0513/public/index.js
@@ -1,39 +1,64 @@
 const root = document.getElementById('root');
 
 
-function catAppend(cats){
+function catAppend() {
 
-  cats = [];
-  
-  for(let i = 0; i<6; i++){
+  for (let i = 0; i < 6; i++) {
     let div = document.createElement('div');
+    let img = document.createElement('img');
     root.appendChild(div);
+    div.appendChild(img);
     // console.dir(div);
-
-    //* 이거 맞나...
-    for(let cat = 3455; cat < 3461; cat++){
-    div.innerHTML = `<img src="../img/IMG_${cat}.PNG" alt="">`
-    cats.push(div);
-    }
   }
-}
+    //* 이거 맞나...
+    // let div = document.getElementById('root').children
+    // for (let cat = 3455; cat < 3461; cat++) {
+    //   let img = document.getElementsByTagName('img')
+    //   for (let i = 0; i < img.length; i++) {
+    //     img[i].src = `../img/IMG_${cat}.PNG`;
+    //     img[i].alt = '';
+    //   }
+    //   // div.innerHTML = `<img src="../img/IMG_${cat}.PNG" alt="">`
+    // }
+
+    let img = document.getElementsByTagName('img')
+    for (let i = 0; i < img.length; i++) {
+      let cat = 3455 + i;
+      img[i].src = `../img/IMG_${cat}.PNG`;
+      img[i].alt = '';
+    }
+
+  // root.children.style.display = 'none';
+
+  }
+
+
+
+const div = document.getElementById('root').children
+//todo 이거 해결해라
+// div.style.display = 'none';
+
+catAppend();
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
   let catCounter = 0;
 
   setInterval(() => {
-  // 이전 텍스트를 숨김
-    h1[catCounter].style.display = 'none'; 
-    // 증가 및 다음 텍스트로 이동, h1.length와 같다면 0
-    catCounter = (catCounter === h1.length - 1) ? 0 : catCounter + 1; 
+    // 이전 텍스트를 숨김
+    div[catCounter].style.display = 'none';
+    // 증가 및 다음 텍스트로 이동, div.length와 같다면 0
+    catCounter = (catCounter === div.length - 1) ? 0 : catCounter + 1;
     // 새로운 텍스트를 표시
-    h1[catCounter].style.display = 'inline'; 
+    div[catCounter].style.display = 'block';
+    console.log('cat');
     // 1초 간격으로 반복
-  }, 1000); 
+  }, 150);
 });
 
 
 
 
-catAppend();
+// catAppend();
 //animation();
