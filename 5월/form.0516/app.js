6dcca01fff7702845060bf904d49aa19b16f2256
moonhr <moonhr153@gmail.com>
2024-05-16 20:26:22 +0900

    post 연구중

--- 5월/form.0516/app.js
+++ 5월/form.0516/app.js
@@ -14,19 +14,30 @@ const server = http.createServer((request, response) => {
       response.end();
     }
 
-    let body = '';
-
-    request.on('data',function(){
-      body += data;
-    });
-
-    request.on('end',function(){
-      let post= qs.parse(body);
-    });
+    if(request.url.startsWith("/test")) {
+      const inputData = request.url.split("?")[1];
+      const data = qs.decode(inputData);
+      console.log(data.type);
+      const name = data.name;
+      const content = data.content;
 
       fs.writeFile( name + '.html', content, (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
+      });
+
+      let body = "";
+      //request - 요청할 때 웹 브라우저가 보낸 정보
+      //response - 응답할 때 웹 브라우저에 보낼 정보
+
+      //데이터를 차례차례 받아  body에 저장.
+      request.on('data',function(){
+        body = body + data;
+      });
+
+      //더 이상 가져올 데이터가 없을 때 실행되는 함수.
+      request.on('end',function(){
+        var post= qs.parse(body);
       });
 
     }
