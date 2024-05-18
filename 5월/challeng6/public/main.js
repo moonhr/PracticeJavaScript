442bf27b1413df8db2a99043207159ff62656dcf
moonhr <moonhr153@gmail.com>
2024-05-18 11:20:08 +0900

    이메일 검사 수정

--- 5월/challeng6/public/main.js
+++ 5월/challeng6/public/main.js
@@ -2,10 +2,9 @@ const root = document.getElementById('root');
 const fieldset = document.getElementsByTagName('fieldset');
 const input = document.getElementsByTagName('input');
 const light = document.getElementsByClassName('light');
-console.log(light);
 
 /**
- * 이름 데이터
+ ** 이름 데이터
  */
 const names = [
   "구하림",
@@ -28,11 +27,11 @@ const names = [
   "조자연",
   "최유진",
   "황재민"
-]; 
+];
 
 
 /**
- * input태그 id 가져오기
+ ** input태그 id 가져오기
  */
 const username = document.getElementById('username');
 const password = document.getElementById('password');
@@ -40,28 +39,36 @@ const password2 = document.getElementById('passwordCheck');
 const email = document.getElementById('email');
 const number = document.getElementById('number');
 
+function greenLight(n) {
+  light[n].style.backgroundColor = 'rgba(74, 255, 81, 1)';
+}
+
+function redLight(n) {
+  light[n].style.backgroundColor = 'rgba(255, 74, 74, 1)';
+}
+
 /**
- ** 이름 검사
+ ** 이름 검사 함수
  */
-username.addEventListener('input', function nameCheck(){
-  for(let name of names){
-    if(username.value === name){
-      light[0].style.backgroundColor = 'rgba(74, 255, 81, 1)';
+function nameCheck() {
+  for (let name of names) {
+    if (username.value === name) {
+      greenLight(0);
       break;
     } else {
-      light[0].style.backgroundColor = 'rgba(255, 74, 74, 1)';
+      redLight(0);
     }
   }
-})
+}
 
 /**
  ** 패스워드 검사
  */
-password.addEventListener('input', function passwordCheck(){
+function passwordCheck() {
   //입력값을 배열로 저장
   let passwordValue = password.value;
   let value = [];
-  for(let i = 0; i<passwordValue.length; i++){
+  for (let i = 0; i < passwordValue.length; i++) {
     value.push(passwordValue[i]);
   }
 
@@ -71,78 +78,86 @@ password.addEventListener('input', function passwordCheck(){
   //forEach로 입력값을 통과하는 문자와 숫자 분리하여 변수에 담기
   value.forEach(element => {
     //inNaN 문자면 true 숫자면 false
-    if(isNaN(element) === true){
+    if (isNaN(element) === true) {
       testName += element;
     } else {
       testNumber += element;
     }
   });
-  
+
+  //불 켜줄 스위치
   let isGreen = false;
 
-  //문자열이 이름에 일치하고 숫자 길이가 4라면 초록함수
+  //문자열이 이름에 일치하고 숫자 길이가 4라면 스위치가 true
   names.forEach(name => {
-    if(name === testName){
-      if(testNumber.length === 4){
+    if (name === testName) {
+      if (testNumber.length === 4) {
         isGreen = true;
       }
     }
   });
-
-  function green(){
-    light[1].style.backgroundColor = 'rgba(74, 255, 81, 1)';
-  }
-
-  function red(){
-    light[1].style.backgroundColor = 'rgba(255, 74, 74, 1)';
-  }
-  
-  if(isGreen === true){
-    green();
+  //스위치가 true라면 초록불 아니라면 빨간불
+  if (isGreen === true) {
+    greenLight(1);
   } else {
-    red();
+    redLight(1);
   }
-})
-
+}
 
 /**
  ** 패스워드 확인 검사
  */
-password2.addEventListener('input', function passwordDoubleCheck(){
-  if(password.value === password2.value){
-    light[2].style.backgroundColor = 'rgba(74, 255, 81, 1)';
+function passwordDoubleCheck() {
+  if (password.value === password2.value) {
+    greenLight(2);
     return;
   } else {
-    light[2].style.backgroundColor = 'rgba(255, 74, 74, 1)';
+    redLight(2);
   }
-})
+}
+
+
+
+
+username.addEventListener('input', nameCheck);
+
+
+password.addEventListener('input', passwordCheck);
+
+
+
+
+password2.addEventListener('input', passwordDoubleCheck);
 
 /**
  ** 이메일 확인 검사
  */
-email.addEventListener('input', function emailCheck(){
-  if(email.value.split('@').length-1){
-    if(email.value.split('.').length-1){
-      if(typeof(email.value.split("@")[0]) === 'string'){
-        if(typeof(email.value.split("@")[1]) === 'string'){
-          if(typeof(email.value.split(".")[1]) === 'string'){
-            if(email.value.indexOf('@')< email.value.indexOf('.')){
-              light[3].style.backgroundColor = 'rgba(74, 255, 81, 1)';
-            } else {
-              light[3].style.backgroundColor = 'rgba(255, 74, 74, 1)';
-            }
-          }
+function emailCheck() {
+  //이메일 구분점
+  let atSplit = email.value.split('@')
+  let dotSplit = email.value.split('.')
+
+  if (atSplit.length == 2 || dotSplit.length == 2) {
+    if (typeof (atSplit[0]) === 'string' || typeof (atSplit[1]) === 'string') {
+      if (typeof (dotSplit[1]) === 'string') {
+        if (email.value.indexOf('@') < email.value.indexOf('.')) {
+          greenLight(3);
+          return;
         }
       }
     }
   }
-})
+  redLight(3);
+}
+
+
+email.addEventListener('input', emailCheck);
 
 /**
  ** 휴대폰 번호 검사
  */
-number.addEventListener('input', function numberCheck(){
-  if(number.value.length === 11){
+number.addEventListener('input', function numberCheck() {
+  if (number.value.length === 11) {
     light[4].style.backgroundColor = 'rgba(74, 255, 81, 1)';
     return;
   } else {
