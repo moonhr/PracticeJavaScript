8cc9f199a69989ff27d9b5f57aec1ed58198a81e
moonhr <moonhr153@gmail.com>
2024-05-23 14:12:41 +0900

    readline으로 응답받기

--- 5월/readLine.0523/app.js
+++ 5월/readLine.0523/app.js
@@ -9,24 +9,53 @@ const readLine = rl.createInterface({
   output : process.stdout
 });
 
-readLine.question("포켓몬스터 이름 작성해주세요 : ", function(answer){
-  let data = [];
-  pokemonData.forEach((element) => {
-    if(answer === element) {
-      data.push(element);
+
+  let answerData = [];
+  let score = 0;
+
+
+function game(){
+readLine.question("포켓몬스터 이름 작성해주세요 :", function(answer){
+
+  // todo : 1. 답변을 받는다.
+  // todo : 2. 답변과 기초데이터와의 존재 유무를 판단한다.
+  // todo : 3. 존재한다면 위 지역변수 배열에 추가한다.(append)
+  // todo : 4. 존재한다면, score를 1점 추가한다.
+  // todo : 5. 존재하지 않는다면, 다시한번 물어본다.
+  // todo : 6. "종료" 라고 말한다면, 질문을 종료하고 결과를 보여준다.
+  // todo : 7. 결과는 총 "포켓몬스터 score 개"를 작성하였습니다. 라는 로그를 남겨준다. 
+    function ball(){
+      pokemonData.forEach(element => {
+        if(answer === element){
+          answerData.push(answer);
+          score++
+          console.log("포켓몬 잡기 성공");
+          console.log(score+"점 획득!");
+          console.log(answerData);
+        } else {
+          console.log("도전은 여기까지")
+          console.log('현재 스코어는'+ score);
+          console.log(answerData)
+          readLine.close();
+        }
+      });
+    }
+    ball();
+
+    if(score === 0){
+      console.log("그런 포켓몬은 없다. 더 공부하고와라")
       readLine.close();
+    } else {
+      readLine.question("다음 포켓몬을 잡겠나? 답은 예/아니오 : ", function(answer2){
+        if(answer2 === '예'){
+          game();
+        } else {
+          console.log('현재 스코어는'+ score);
+          console.log(answerData)
+          readLine.close();
+        }
+      })
     }
   });
-  console.log(data);
-  if(data.length === 0) {
-    console.log("포켓몬스터가 존재하지 않습니다.");
-  } else {
-    console.log("포켓몬스터가 존재합니다.");
-    fs.writeFileSync('./searData.json', JSON.stringify(data), 'utf8', (err) => {
-      if(err) {
-        console.log(err);
-      }
-    });
-  }
-  readLine.close();
-});
+};
+game();
