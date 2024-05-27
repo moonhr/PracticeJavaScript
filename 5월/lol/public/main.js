6319eaafd04d28d68d50bac0eee76a7e2e4b38ff
moonhr <moonhr153@gmail.com>
2024-05-27 16:50:14 +0900

    add function lineCheck

--- 5월/lol/public/main.js
+++ 5월/lol/public/main.js
@@ -4,18 +4,19 @@ import { memberNames } from "../module/namedata.js"
 // import { chamCheck } from "../module/chamCheck.js";
 // const nameCheck = require("node:fs"); 이건 Node방식
 
-const nameInput = document.getElementById("name");
+
 const chamInput = document.getElementById("cham");
-const lineInput = document.getElementById("line");
 const input = document.getElementById("submit");
 
 
 //* addEventListener는 함수를 보내주지 않으면 다시 호출되지 않음.
+const nameInput = document.getElementById("name");
+const lineInput = document.getElementById("line");
+
+
 
 function nameCheck(){
-
   let nameMenu = document.getElementById('name.menu')
-
   //includes메서드는 배열에 ()가 있는지 참거짓을 테스트함
   let student = memberNames.includes(nameInput.value)
 
@@ -29,10 +30,21 @@ function nameCheck(){
   }
 }
 
+function lineCheck(){
+  let lineMenu = document.getElementById("line.menu");
 
+  console.log("lineCheck 작동중")
+  if(lineInput){
+    lineMenu.style.color = "rgba(255,255,255,1)";
+  } else {
+    lineMenu.style.color = "rgba(255,255,255,0.2)";
+  }
+}
 
 
 nameInput.addEventListener('blur',nameCheck);
+lineInput.addEventListener('change',lineCheck);
+
 
 
 console.log("ㅏㅏ");
